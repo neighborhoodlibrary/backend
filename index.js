979bcf6dcdef0aa28aa9fc3b0333345f5a949c59
jeffyjkang <jeffyjkang@gmail.com>
2019-07-29 22:58:23 -0700

    update email functionality

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const server = express();
+// twilio send grid
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 //
 server.use(express.json());
 server.use(helmet());
@@ -13,6 +16,28 @@ server.use(cors());
 // test for base url
 server.get("/", (req, res) => {
   res.send("<h1>Server Running<h1>");
+});
+
+server.post("/email", async (req, res) => {
+  const msg = req.body;
+  if (!msg.to) {
+    return res.status(400).json({ error: "Must provide email to send to." });
+  }
+  if (!msg.from) {
+    return res.status(400).json({ error: "Must provide email to send from." });
+  }
+  if (!msg.subject) {
+    return res.status(400).json({ error: "Must provide subject matter." });
+  }
+  if (!msg.text) {
+    return res.status(400).json({ error: "Must provide text body." });
+  }
+  try {
+    const response = sgMail.send({ ...msg });
+    res.status(201).json("Email sent");
+  } catch (error) {
+    res.status(500).json({ error: "There was a problem sending the email" });
+  }
 });
 
 //
