edc589b276d16be7899f61c81366973b50c42d28
jeffyjkang <jeffyjkang@gmail.com>
2019-08-07 14:40:56 -0700

    have back send info of goodreads api to front

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const server = express();
+// request promise
+const rp = require("request-promise");
+// xml parser
+const parseString = require("xml2js").parseString;
+// goodreads api
+const goodreadsKey = process.env.GOODREADS_API_KEY;
 // twilio send grid
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -40,6 +46,38 @@ server.post("/email", async (req, res) => {
     res.status(201).json("Email sent");
   } catch (error) {
     res.status(500).json({ error: "There was a problem sending the email" });
+  }
+});
+//
+server.post("/goodreads", async (req, res) => {
+  const body = req.body;
+  if (!body.query) {
+    return res.status(400).json({ error: "Must provide query" });
+  }
+  if (!body.search) {
+    return res.status(400).json({ error: "Must provide search type" });
+  }
+  try {
+    let gUrl = "https://www.goodreads.com/search/index.xml?";
+    let key = `key=${goodreadsKey}`;
+    let query = `q=${body.query}`;
+    let search = `search%5bfield%5d=${body.search}`;
+    let URL = `${gUrl}${key}&${query}&${search}`;
+    let returnRes = await rp(URL)
+      .then(res => {
+        parseString(res, (err, result) => {
+          res = result.GoodreadsResponse.search[0].results[0].work;
+        });
+        return res;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    res.status(200).json(returnRes);
+  } catch (error) {
+    res.status(500).json({
+      error: "There was a problem sending request to the goodreads api"
+    });
   }
 });
 
